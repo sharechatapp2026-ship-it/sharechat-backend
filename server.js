2627f68f0f38c5a756580262200440e88fee74d7
Ilyas Bouderhem <ilyasbouderhem2002@gmail.com>
2026-03-26 21:55:36 +0100

    Change default port to 8080 for Railway

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const __dirname = path.dirname(__filename)
 dotenv.config()
 
 const app = express()
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 8080
 
 // Middleware
 app.use(cors())
